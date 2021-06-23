2e26aae2ae53940c0790cb7401ce14c3e7d942cd
Wade Facer <wadepfacer@gmail.com>
2021-06-23 17:32:35 -0600

    polished handlebars route to generate page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.use('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.use('/', (req, res) => {
+  res.render('index', { title: 'Got Gear' });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const routes = require('./routes');
 const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT||3001;
-const exhandlebars = require('handlebars');
+const exhandlebars = require('express-handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exhandlebars());
+app.engine('handlebars', exhandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.use(cookieParser());
@@ -17,13 +19,14 @@ app.use(cookieParser());
 
 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+sequelize.sync({ force: true }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));    
+})
+.catch(err => {
+    console.log(err)
 });
