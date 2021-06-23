f6eef1c8af5cb63b3c4cbd22bb34fdd7477379e7
Malcolm Easton <eastonmalcolm@gmail.com>
2021-06-22 22:49:16 -0600

    DB_NAME should always be got_gear_db, not in .env

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
+const DB_NAME = 'got_gear_db';
 
 // Create connection to our databse using mysql info.
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+const sequelize = new Sequelize(DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: process.env.DB_ADDRESS || 'localhost',
     dialect: 'mysql',
     port: process.env.DB_PORT || 3306
