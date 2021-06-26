99d2752b91d87a91a270030d44b48b7f325baa0a
Malcolm Easton <eastonmalcolm@gmail.com>
2021-06-25 22:03:57 -0600

    added gear model and routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const { resourceLimits } = require('worker_threads');
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const gearRoutes = require('./gear-routes');
 
 router.use('/users', userRoutes);
+router.use('/gear', gearRoutes);
 
 module.exports = router;
