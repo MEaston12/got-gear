cf16d25bf8668741549f198304f09d5e186d67f5
Malcolm Easton <eastonmalcolm@gmail.com>
2021-06-27 17:36:51 -0600

    changed relation of gear-user from m-1 to m-m

--- models/Gear.js
+++ models/Gear.js
@@ -12,10 +12,6 @@ Gear.init(
             primaryKey: true,
             autoIncrement: true
         },
-        user_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
         name: {
             type: DataTypes.STRING,
             allowNull: false
